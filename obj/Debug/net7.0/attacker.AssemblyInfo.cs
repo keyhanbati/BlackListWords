b16a6707787975d6bc31680d1ac6dbee330b15f2
k.shafaat <k.shafaat@tajan.tech>
2024-07-23 15:54:04 +0330

    first commit

--- obj/Debug/net7.0/attacker.AssemblyInfo.cs
+++ obj/Debug/net7.0/attacker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("attacker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5619c1eb775b4aa6df9a0f56f928b47cae6f576f")]
 [assembly: System.Reflection.AssemblyProductAttribute("attacker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("attacker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
